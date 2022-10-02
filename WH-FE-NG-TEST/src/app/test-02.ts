92ef8fdbd5fe61887ff32cb63e2f48357972ce82
SokoPaulSokool <sopapaso73@gmail.com>
2022-10-02 17:53:38 +0300

    angular test 02

--- WH-FE-NG-TEST/src/app/test-02.ts
+++ WH-FE-NG-TEST/src/app/test-02.ts
@@ -3,49 +3,65 @@
  * * Bind `field` of [textfield] component to its text input
  * * Pass value of `field` from [textfield] component to [title] property of component [ng-app]
  */
-import { Component, NgModule  } from '@angular/core';
-import { RouterModule } from "@angular/router";
-import { CommonModule } from '@angular/common';
-
-@Component({
-    selector : 'textfield',
-    template : '<input type="text" value="" />'
-})
-export class TextField {
-    field = "";
-}
-
-@Component({
-    selector : 'child-component',
-    template : `<h2>Title:<h2><br/><textfield></textfield>`
-})
-export class ChildComponent {
-
-}
-
-
-@Component({
-    selector : 'ng-app',
-    template : `<div>
-                    <child-component></child-component> <br/>
-                    Title is {{title}}
-                </div>`
-})
-export class Test02Component {
-
-    title:string = "";
-}
-
-@NgModule({
-    imports : [
-        CommonModule,
-        RouterModule.forChild([
-            {
-                path : "",
-                component : Test02Component
-            }
-        ])
-    ],
-    declarations : [Test02Component,ChildComponent,TextField]
-})
-export class Test02Module {};
+ import { Component, EventEmitter, NgModule, Output  } from '@angular/core';
+ import { RouterModule } from "@angular/router";
+ import { CommonModule } from '@angular/common';
+ 
+ @Component({
+     selector : 'textfield',
+     template : '<input type="text" [value]="field"  (input)="onValueChange($event.target.value)"  /> '
+ })
+ export class TextField {
+      field = "";
+ 
+     @Output() onFieldChange: EventEmitter<any> = new EventEmitter<any>();
+ 
+     onValueChange(val:any){
+       this.onFieldChange.emit(val); 
+     }
+ }
+ 
+ @Component({
+     selector : 'child-component',
+     template : `<h2>Title:<h2><br/><textfield (onFieldChange)="onChange($event)"></textfield>`
+ })
+ export class ChildComponent {
+     title=""
+     @Output() onTitleChange: EventEmitter<any> = new EventEmitter<any>();
+ 
+     onChange(val:any){
+         this.title=val
+         this.onTitleChange.emit(val);
+     }
+ 
+ }
+ 
+ 
+ @Component({
+     selector : 'ng-app',
+     template : `<div>
+                     <child-component (onTitleChange)="onChange($event)"></child-component> <br/>
+                     Title is {{title}}
+                 </div>`
+ })
+ export class Test02Component {
+ 
+     title = "";
+     onChange(val:any){
+         this.title= val
+     }
+ }
+ 
+ @NgModule({
+     imports : [
+         CommonModule,
+         RouterModule.forChild([
+             {
+                 path : "",
+                 component : Test02Component
+             }
+         ])
+     ],
+     declarations : [Test02Component,ChildComponent,TextField]
+ })
+ export class Test02Module {};
