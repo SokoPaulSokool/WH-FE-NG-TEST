8151e6f749c87a75919f87e9e80ace171bb4a237
SokoPaulSokool <sopapaso73@gmail.com>
2022-10-02 18:18:42 +0300

    react test 02

--- WH-FE-NG-TEST/jsx-tests/src/test-02.tsx
+++ WH-FE-NG-TEST/jsx-tests/src/test-02.tsx
@@ -3,25 +3,66 @@
  * You are free to add classes and styles, but make sure you leave the element ID's as they are.
  */
 
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div id="mainArea">
-        <p>button count: <span>0</span></p>
-        <button id="mainButton">Increase</button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Counter />,
-  document.getElementById('test-02')
-);
+ import React, {  } from 'react';
+ import ReactDOM from 'react-dom';
+ 
+ const style = {
+   countText: {
+     fontSize: 30,
+     color: "white",
+     marginLeft: 6,
+     borderWidth: 2,
+     borderColor: "lightseagreen",
+     background: "lightseagreen",
+     borderStyle: "solid",
+     borderRadius: 6,
+     padding: 10,
+   },
+   paragraph: {
+     textAlign: "center",
+     display: "flex",
+     alignItems: "center",
+   },
+   counterBtn: {
+     marginTop: "10px",
+     padding: "10px 15px",
+     border: "none",
+     backgroundColor: "lightseagreen",
+     fontSize: "14px",
+     borderRadius: "5px",
+   },
+ } as const;
+ 
+ class Counter extends React.Component {
+   state = { counter: 0 };
+   constructor(props) {
+     super(props);
+   }
+ 
+   handleCounterIncrease = () => {
+     this.setState({ counter: this.state.counter + 1 });
+   };
+ 
+   render() {
+     return (
+       <div id="mainArea">
+         <p style={style.paragraph}>
+           button count:{" "}
+           <span style={style.countText}>{this.state.counter}</span>
+         </p>
+         <button
+           id="mainButton"
+           onClick={this.handleCounterIncrease}
+           style={style.counterBtn}
+         >
+           Increase
+         </button>
+       </div>
+     );
+   }
+ }
+ 
+ ReactDOM.render(
+   <Counter />,
+   document.getElementById('test-02')
+ );
