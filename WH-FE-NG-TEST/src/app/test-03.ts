440f43d1adef95c62cf6b0ed9bd456f015a5c632
SokoPaulSokool <sopapaso73@gmail.com>
2022-10-02 18:05:19 +0300

    angular test 03

--- WH-FE-NG-TEST/src/app/test-03.ts
+++ WH-FE-NG-TEST/src/app/test-03.ts
@@ -12,41 +12,103 @@
  * 
  * You can add error messages below each field that shows if the field is not valid
  */
-import { Component, NgModule  } from '@angular/core';
-import { RouterModule } from "@angular/router";
-import { CommonModule } from '@angular/common';
-
-@Component({
-    selector : 'ng-app',
-    template : `<form>
-                    <h2>Login</h2>
-                    <br/>
-                    <input type="email" value="" name="email" />
-                    <br/>
-                    <input type="password" value="" name="password" />
-                    <button type="submit">Submit</button>
-                    <br/><br/>
-                    <div *ngIf="logged_in">Logged In!</div>
-                </form>`
-})
-export class Test03Component {
-
-    email:string = "";
-    password:string = "";
-
-    logged_in = false;
-}
-
-@NgModule({
-    imports : [
-        CommonModule,
-        RouterModule.forChild([
-            {
-                path : "",
-                component : Test03Component
-            }
-        ])
-    ],
-    declarations : [Test03Component]
-})
-export class Test03Module {};
+ import { Component, NgModule  } from '@angular/core';
+ import { RouterModule } from "@angular/router";
+ import { CommonModule } from '@angular/common';
+ 
+ @Component({
+   selector: "ng-app",
+   template: `<form (submit)="onSubmit($event)">
+     <h2>Login</h2>
+     <br />
+     <input
+       type="email"
+       name="email"
+       [value]="email"
+       (change)="emailChange($event.target.value)"
+     />
+ 
+     <p
+       *ngIf="emailErrorMessage"
+       [ngStyle]="{ color: 'red', fontSize: '0.7em' }"
+     >
+       {{ emailErrorMessage }}
+     </p>
+     <br />
+     <input
+       autocomplete=""
+       [(value)]="password"
+       name="password"
+       (change)="passwordChange($event.target.value)"
+     />
+     <p
+       *ngIf="passwordErrorMessage"
+       [ngStyle]="{ color: 'red', fontSize: '0.7em' }"
+     >
+       {{ passwordErrorMessage }}
+     </p>
+     <button type="submit">Submit</button>
+     <br /><br />
+     <div *ngIf="logged_in">Logged In!</div>
+   </form>`,
+ })
+ export class Test03Component {
+   email: string = "";
+   password: string = "";
+ 
+   logged_in = false;
+ 
+   emailErrorMessage = "";
+   passwordErrorMessage = "";
+ 
+   onSubmit(e) {
+     e.preventDefault();
+     if (
+       this.isEmailAddressValid(this.email) &&
+       this.isPasswordValid(this.password)
+     ) {
+       this.logged_in = true;
+       this.emailErrorMessage = "";
+      this.passwordErrorMessage = "";
+     }
+   }
+   emailChange(email) {
+     this.email = email;
+     if (!this.isEmailAddressValid(this.email)) {
+       this.emailErrorMessage = "Invalid Email";
+     } else {
+       this.emailErrorMessage = "";
+     }
+   }
+   passwordChange(password) {
+     this.password = password;
+     if (!this.isEmailAddressValid(this.password)) {
+       this.passwordErrorMessage = "Invalid Password";
+     } else {
+       this.passwordErrorMessage = "";
+     }
+   }
+   isEmailAddressValid(str) {
+     var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+     return pattern.test(str);
+   }
+   isPasswordValid(str) {
+     var pattern =
+       /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{7,}$/;
+     return pattern.test(str);
+   }
+ }
+ 
+ @NgModule({
+     imports : [
+         CommonModule,
+         RouterModule.forChild([
+             {
+                 path : "",
+                 component : Test03Component
+             }
+         ])
+     ],
+     declarations : [Test03Component]
+ })
+ export class Test03Module {};
