8e04e6c3f678379853445ebcbbc668dc4faedee9
SokoPaulSokool <sopapaso73@gmail.com>
2022-10-02 18:20:23 +0300

    react test 03

--- WH-FE-NG-TEST/jsx-tests/src/test-03.tsx
+++ WH-FE-NG-TEST/jsx-tests/src/test-03.tsx
@@ -10,106 +10,173 @@
  * 
  */
 
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
-const style = {
-    table: {
-        borderCollapse: "collapse"
-    },
-    tableCell: {
-        border: '1px solid gray',
+    import React, { useState } from 'react';
+    import ReactDOM from 'react-dom';
+    
+    const style = {
+      table: {
+        borderCollapse: "collapse",
+      },
+      tableCell: {
+        border: "1px solid gray",
         margin: 0,
-        padding: '5px 10px',
-        width: 'max-content',
-        minWidth: '150px'
-    },
-    form: {
+        padding: "5px 10px",
+        width: "max-content",
+        minWidth: "150px",
+      },
+      form: {
         container: {
-            padding: '20px',
-            border: '1px solid #F0F8FF',
-            borderRadius: '15px',
-            width: 'max-content',
-            marginBottom: '40px'
+          padding: "20px",
+          border: "1px solid #F0F8FF",
+          borderRadius: "15px",
+          width: "max-content",
+          marginBottom: "40px",
         },
         inputs: {
-            marginBottom: '5px'
+          marginBottom: "5px",
         },
         submitBtn: {
-            marginTop: '10px',
-            padding: '10px 15px',
-            border: 'none',
-            backgroundColor: 'lightseagreen',
-            fontSize: '14px',
-            borderRadius: '5px'
-        }
+          marginTop: "10px",
+          padding: "10px 15px",
+          border: "none",
+          backgroundColor: "lightseagreen",
+          fontSize: "14px",
+          borderRadius: "5px",
+        },
+      },
+    } as const;
+    
+    interface Contact {
+      userFirstName: string;
+      userLastName: string;
+      userPhone: string;
     }
-} as const;
-
-function PhoneBookForm({ addEntryToPhoneBook }) {
-    return (
-        <form onSubmit={e => { e.preventDefault() }} style={style.form.container}>
-            <label>First name:</label>
-            <br />
-            <input
-                style={style.form.inputs}
-                className='userFirstname'
-                name='userFirstname'
-                type='text'
-            />
-            <br />
-            <label>Last name:</label>
-            <br />
-            <input
-                style={style.form.inputs}
-                className='userLastname'
-                name='userLastname'
-                type='text'
-            />
-            <br />
-            <label>Phone:</label>
-            <br />
-            <input
-                style={style.form.inputs}
-                className='userPhone'
-                name='userPhone'
-                type='text'
-            />
-            <br />
-            <input
-                style={style.form.submitBtn}
-                className='submitButton'
-                type='submit'
-                value='Add User'
-            />
+    
+    
+    function PhoneBookForm({
+      addEntryToPhoneBook,
+    }) {
+      const [userFirstName, setUserFirstName] = useState("Coder");
+      const [userLastName, setUserLastName] = useState("Byte");
+      const [userPhone, setUserPhone] = useState("8885559999");
+    
+      return (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            addEntryToPhoneBook({ userFirstName, userLastName, userPhone });
+          }}
+          style={style.form.container}
+        >
+          <label>First name:</label>
+          <br />
+          <input
+            style={style.form.inputs}
+            className="userFirstname"
+            name="userFirstname"
+            type="text"
+            value={userFirstName}
+            onChange={(e) => {
+              setUserFirstName(e.target.value);
+            }}
+          />
+          <br />
+          <label>Last name:</label>
+          <br />
+          <input
+            style={style.form.inputs}
+            className="userLastname"
+            name="userLastname"
+            type="text"
+            value={userLastName}
+            onChange={(e) => {
+              setUserLastName(e.target.value);
+            }}
+          />
+          <br />
+          <label>Phone:</label>
+          <br />
+          <input
+            style={style.form.inputs}
+            className="userPhone"
+            name="userPhone"
+            type="text"
+            value={userPhone}
+            onChange={(e) => {
+              setUserPhone(e.target.value);
+            }}
+          />
+          <br />
+          <input
+            style={style.form.submitBtn}
+            className="submitButton"
+            type="submit"
+            value="Add User"
+          />
         </form>
-    )
-}
-
-function InformationTable(props) {
-    return (
-        <table style={style.table} className='informationTable'>
+      );
+    }
+    
+    function InformationTable(props) {
+        return (
+          <table style={style.table} className="informationTable">
             <thead>
-                <tr>
-                    <th style={style.tableCell}>First name</th>
-                    <th style={style.tableCell}>Last name</th>
-                    <th style={style.tableCell}>Phone</th>
-                </tr>
+              <tr>
+                <th style={style.tableCell}>First name</th>
+                <th style={style.tableCell}>Last name</th>
+                <th style={style.tableCell}>Phone</th>
+              </tr>
             </thead>
-        </table>
+            <tbody>
+              {props.contacts.map((contact:Contact, i) => {
+                return (
+                  <tr key={"oo" + i}>
+                    <th style={style.tableCell}>{contact.userFirstName}</th>
+                    <th style={style.tableCell}>{contact.userLastName}</th>
+                    <th style={style.tableCell}>{contact.userPhone}</th>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        );
+    }
+    
+    function Application(props) {
+        const [contacts, setContacts]=useState<Contact[]>([])
+    
+        const handleAddEntryToPhoneBook=({userFirstName, userLastName, userPhone}:Contact)=>{
+            const newContact = { userFirstName, userLastName, userPhone };
+            let updatedContactList =[...contacts]
+            const contactsCheck = contacts.filter(
+              (contact) =>
+                contact.userFirstName === userFirstName &&
+                contact.userLastName === userLastName &&
+                contact.userPhone === userPhone
+            );
+            if (contactsCheck.length > 0) {
+            } else {
+              updatedContactList = [...updatedContactList, newContact];
+            }
+            updatedContactList=updatedContactList.sort((contactA,contactB)=>{
+                if (contactA.userLastName > contactB.userLastName) {
+                  return 1;
+                } else {
+                  return -1;
+                }
+            })
+            setContacts([...updatedContactList]);
+    
+        }
+        return (
+          <section>
+            <PhoneBookForm addEntryToPhoneBook={handleAddEntryToPhoneBook} />
+            <InformationTable contacts={contacts} />
+          </section>
+        );
+    }
+    
+    ReactDOM.render(
+        <Application />,
+        document.getElementById('test-03')
     );
-}
-
-function Application(props) {
-    return (
-        <section>
-            <PhoneBookForm addEntryToPhoneBook="" />
-            <InformationTable />
-        </section>
-    );
-}
-
-ReactDOM.render(
-    <Application />,
-    document.getElementById('test-03')
-);
